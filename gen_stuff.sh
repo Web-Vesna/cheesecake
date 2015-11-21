6240458a3be75eeb2a1b8be86d8b995e620ca1f2
Pavel Berezhnoy <pberejnoy2005@gmail.com>
2015-11-21 15:52:03 +0300

    Man file generating fix

--- gen_stuff.sh
+++ gen_stuff.sh
@@ -2,5 +2,7 @@
 
 set -x # make verbose
 
+FNAME=$(basename "$0")
+
 pod2markdown README.pod > README.md
-git ls-tree --full-tree -r HEAD | awk '{print $4}' | egrep -v "^\.|README.md|$0" > MANIFEST
+git ls-tree --full-tree -r HEAD | awk '{print $4}' | egrep -v "^\.|README.md|$FNAME" > MANIFEST
