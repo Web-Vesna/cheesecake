b1fee56900b8a54cf018d23c89ba00a1f1572f81
Pavel Berezhnoy <pberejnoy2005@gmail.com>
2015-11-29 01:59:09 +0300

    Tests fix

--- gen_stuff.sh
+++ gen_stuff.sh
@@ -5,4 +5,4 @@ set -x # make verbose
 FNAME=$(basename "$0")
 
 pod2markdown README.pod > README.md
-git ls-tree --full-tree -r HEAD | awk '{print $4}' | egrep -v "^\.|README.md|$FNAME" > MANIFEST
+git ls-tree --full-tree -r HEAD | awk '{print $4}' | egrep -v "^\.|README.md|$FNAME|\.t$" > MANIFEST
